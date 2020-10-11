1960a2cfa289b6822db30d2a17b3f143b7e2becb
MichaelHo <michelo851a1203@gmail.com>
2020-10-11 16:38:41 +0800

    init

---
+++ src/components/Scanner.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="scanqrcode">
+    <video class="border border-black w-full h-full" ref="video"></video>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { ref, watch, onUnmounted } from "vue";
+import { BrowserQRCodeReader } from "@zxing/library";
+
+export default {
+  name: "scanqrcode",
+  setup() {
+    const codeReader = new BrowserQRCodeReader();
+    const cameraRef = ref("");
+    const video = ref(null);
+    codeReader
+      .listVideoInputDevices()
+      .then((videoInputDevices) => {
+        if (videoInputDevices.length === 0) {
+          return;
+        }
+        cameraRef.value = videoInputDevices[0].deviceId;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    watch(cameraRef, (deviceId) => {
+      if (deviceId === "") {
+        return;
+      }
+      codeReader
+        .decodeOnceFromVideoDevice(deviceId, video.value)
+        .then((result) => {
+          console.group(`%c result`, "color:yellow");
+          console.log(result.text);
+          console.groupEnd();
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+
+    onUnmounted(() => {
+      codeReader.reset();
+    });
+    return { video };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Scanner></Scanner>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Scanner from "@/components/Scanner.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Scanner,
+  },
+};
 </script>
