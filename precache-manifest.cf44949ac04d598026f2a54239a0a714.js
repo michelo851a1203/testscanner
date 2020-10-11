2b6dd0f8fdda3cff46a6e22e006f21fca2953a59
MichaelHo <michelo851a1203@gmail.com>
2020-10-11 17:05:50 +0800

    deploy

---
+++ precache-manifest.cf44949ac04d598026f2a54239a0a714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30cc2a38a80770ba29f213ad44f3c301",
+    "url": "/testscanner/index.html"
+  },
+  {
+    "revision": "dee0a561104b21a56ea7",
+    "url": "/testscanner/js/about.0719e97a.js"
+  },
+  {
+    "revision": "101ac5bb7eba839a6449",
+    "url": "/testscanner/js/app.c6626233.js"
+  },
+  {
+    "revision": "14e51b7d0e52b197309b",
+    "url": "/testscanner/js/chunk-vendors.fe786ee4.js"
+  },
+  {
+    "revision": "8865d0d48ed74c350b193f3e15775349",
+    "url": "/testscanner/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/testscanner/robots.txt"
+  }
+]);
